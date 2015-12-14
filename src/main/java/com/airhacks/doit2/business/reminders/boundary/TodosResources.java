c32b4793e2596e6e86e0a126b68a6b0cff4e752b
Marcin Zaremba <marcinzaremba@gmail.com>
2015-12-14 23:22:04 +0100

    Tests

--- src/main/java/com/airhacks/doit2/business/reminders/boundary/TodosResources.java
+++ src/main/java/com/airhacks/doit2/business/reminders/boundary/TodosResources.java
@@ -2,16 +2,38 @@ package com.airhacks.doit2.business.reminders.boundary;
 
 import com.airhacks.doit2.business.reminders.entity.ToDo;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("todos")
 public class TodosResources {
 
     @GET
-    public ToDo hello()
+    @Path("{id}")
+    public ToDo find(@PathParam("id") long id)
     {
-        return new ToDo("caption", "...", 100);
-        //return "test";
+        return new ToDo("implement REST endpoint " + id, "...", 100);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void delete(@PathParam("id") long id)
+    {
+        System.out.println("deleted = " + id);
+    }
+
+    @GET
+    public List<ToDo> all()
+    {
+        List<ToDo> all = new ArrayList<>();
+        all.add(find(42));
+        return all;
+    }
+
+    @POST
+    public void save(ToDo todo)
+    {
+        System.out.println("todo = " + todo);
     }
 }
---
+++ src/test/java/com/airhacks/doit/business/reminders/boundary/TodosResources.java
@@ -0,0 +1,37 @@
+package com.airhacks.doit.business.reminders.boundary;
+
+import com.airhacks.rulz.jaxrsclient.JAXRSClientProvider;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class TodosResources {
+    @Rule
+    public JAXRSClientProvider provider = JAXRSClientProvider.buildWithURI("http://localhost:8080/doit2/api/todos");
+
+    @Test
+    public void crud()
+    {
+        Response response = this.provider.target().request(MediaType.APPLICATION_JSON).get();
+        Assert.assertThat(response.getStatus(), CoreMatchers.is(200));
+        JsonArray allTodos = response.readEntity(JsonArray.class);
+        System.out.println("allTodos " + allTodos);
+        Assert.assertFalse(allTodos.isEmpty());
+
+        JsonObject todo = allTodos.getJsonObject(0);
+        Assert.assertTrue(todo.getString("caption").startsWith("imp"));
+
+        //GET with id
+        JsonObject dedicatedTodo = this.provider.target().path("42").request(MediaType.APPLICATION_JSON).get(JsonObject.class);
+        Assert.assertTrue(dedicatedTodo.getString("caption").contains("42"));
+
+        Response deleteResponse = this.provider.target().path("42").request(MediaType.APPLICATION_JSON).delete();
+        Assert.assertThat(deleteResponse.getStatus(), CoreMatchers.is(204)); //status 204 ma void
+    }
+}
