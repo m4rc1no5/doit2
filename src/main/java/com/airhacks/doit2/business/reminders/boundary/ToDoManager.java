d410d69dd81e45e50a6531080e8f50426474ab3f
Marcin Zaremba <marcinzaremba@gmail.com>
2015-12-17 21:59:44 +0100

    Back to good point

--- src/main/java/com/airhacks/doit2/business/reminders/boundary/ToDoManager.java
+++ src/main/java/com/airhacks/doit2/business/reminders/boundary/ToDoManager.java
@@ -3,32 +3,31 @@ package com.airhacks.doit2.business.reminders.boundary;
 import com.airhacks.doit2.business.reminders.entity.ToDo;
 
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
 
 @Stateless
 public class ToDoManager {
 
-    @PersistenceContext
-    EntityManager em;
-
     public ToDo findById(long id) {
-        return this.em.find(ToDo.class, id);
+        //return this.em.find(ToDo.class, id);
+        return new ToDo("implement REST point " + id, "...", 100);
     }
 
     public void delete(long id) {
-        ToDo reference = this.em.getReference(ToDo.class, id);
-        this.em.remove(reference);
+        //ToDo reference = this.em.getReference(ToDo.class, id);
+        //this.em.remove(reference);
     }
 
     public List<ToDo> all() {
-        return this.em.createNamedQuery(ToDo.findAll, ToDo.class).getResultList();
+        //return this.em.createNamedQuery(ToDo.findAll, ToDo.class).getResultList();
+        List<ToDo> all = new ArrayList<>();
+        all.add(findById(42));
+        return all;
     }
 
     public void save(ToDo todo)
     {
-        this.em.merge(todo);
+        //this.em.merge(todo);
     }
 }
